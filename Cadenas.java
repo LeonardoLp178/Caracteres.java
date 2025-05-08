87d1a6d5a0982bf3a457584070883f730dacd900
leo <20243ds178@utez.edu.mx>
2025-05-08 09:33:43 -0600

    agregue nombre y apellido

---
+++ Cadenas.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Cadena{
+    public static void main(String[] args){
+        String nombre;
+        Scanner entrada = new Scanner (System.in);
+        System.out.println("escribe tu nombre ");
+        String nombre = entrada.nextLine();
+
+        String apellido;
+        Scanner entrada = new Scanner (System.in);
+        System.out.println("escribe tu apellido ");
+        String apellido = entrada.nextLine();
+    }
+}
