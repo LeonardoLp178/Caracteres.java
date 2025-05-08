ab7662eec59d1188b2304687f5982702dbec343e
leo <20243ds178@utez.edu.mx>
2025-05-08 09:42:30 -0600

    ya acabe

--- Cadenas.java
+++ Cadenas.java
@@ -1,14 +1,10 @@
 import java.util.Scanner;
-public class Cadena{
+public class Cadenas{
     public static void main(String[] args){
         String nombre;
-        Scanner entrada = new Scanner (System.in);
-        System.out.println("escribe tu nombre ");
-        String nombre = entrada.nextLine();
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("escribe tu nombre");
+         nombre = entrada.nextLine();
 
-        String apellido;
-        Scanner entrada = new Scanner (System.in);
-        System.out.println("escribe tu apellido ");
-        String apellido = entrada.nextLine();
     }
 }
